a974100337f92a50c13b142862163ff75f636ff8
hsx02 <hsx02self@gmail.com>
2021-12-18 13:30:47 +0530

    pine: Welcome SparkOS!!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The SparkOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pine.mk
+    $(LOCAL_DIR)/spark_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pine-user \
-    lineage_pine-userdebug \
-    lineage_pine-eng
+    spark_pine-user \
+    spark_pine-userdebug \
+    spark_pine-eng
